4830ff86267117ff493d121e08cd032e1fc550bd
Jason Oh <jasonoh1998@gmail.com>
2022-11-07 15:21:33 +0900

    Tomcat 9.0 javax module changes

--- src/main/java/com/control/CommandProcess.java
+++ src/main/java/com/control/CommandProcess.java
@@ -1,7 +1,7 @@
 package com.control;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public interface CommandProcess {
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable;
--- src/main/java/video/service/VideoHomeService.java
+++ src/main/java/video/service/VideoHomeService.java
@@ -2,8 +2,8 @@ package video.service;
 
 import com.control.CommandProcess;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class VideoHomeService implements CommandProcess {
 
