55e5967ca989409096c86dc4059a5051f258767d
heon <ghdgjs3027@gmail.com>
2022-11-24 17:29:13 +0900

    violet display1

--- src/main/java/home/controller/HomeController.java
+++ src/main/java/home/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -19,4 +20,36 @@ public class HomeController { // this is a videoController
 		model.addAttribute("page", "video");
 		return "index";
 	}
+	
+
 }
+
+
+
+/*
+ * @GetMapping(value="/video/movie/basic_Contents") public String
+ * movieBasicContents() {
+ * 
+ * return "video/movie/basic_Contents"; }
+ */
+
+/*
+ * @GetMapping(value="/video/movie/category_Contents") public String
+ * movieCategoryContents() {
+ * 
+ * return "video/movie/category_Contents"; }
+ * 
+ * @GetMapping(value="/video/tv_Program/basic_Contents") public String
+ * tvBasicContents() {
+ * 
+ * return "video/tv_Program/basic_Contents"; }
+ * 
+ * @GetMapping(value="/video/tv_Program/category_Contents") public String
+ * tvCategoryContents() {
+ * 
+ * return "video/tv_Program/category_Contents"; }
+ * 
+ * @GetMapping(value="/video/tv_Program/tvshow") public String tvshow() {
+ * 
+ * return "video/tv_Program/tvshow"; }
+ */
