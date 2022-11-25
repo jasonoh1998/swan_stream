cc1b4acb91b21f02ab1fa2bf375b7657384ae0c5
Jason Oh <jasonoh1998@gmail.com>
2022-11-25 16:56:05 +0900

    made login page show error page

--- src/main/java/main/controller/MainController.java
+++ src/main/java/main/controller/MainController.java
@@ -1,5 +1,7 @@
 package main.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
@@ -7,38 +9,54 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import main.bean.MainDTO;
 import main.service.MainService;
 
 @Component
 @Controller
+@SessionAttributes("email")
 public class MainController {
 	@Autowired
 	private MainService mainService;
 	
 	@GetMapping(value="/")
-	public String main() {
-		return "login/main";
+	public String main(Model model) {
+		if(model.getAttribute("email")==null) {
+			return "login/main";		
+		} else {
+			return "index";
+		}
 	}
 	
 	@GetMapping(value="/login")
-	public String login() {
-		return "login/login";
+	public String login(Model model) {
+		if(model.getAttribute("email")==null) {
+			return "login/login";		
+		} else {
+			return "index";
+		}
 	}
 	
 	@GetMapping(value="/signUp") // calls /mainUserDataSave to save user data redirects to /login
-	public String signUp() {
-		return "login/signUp";
+	public String signUp(Model model) {
+		if(model.getAttribute("email")==null) {
+			return "login/signUp";	
+		} else {
+			return "index";
+		}
 	}
 	
 	@GetMapping(value="/findPassword")
-	public String findPassword() {
-		return "login/findPassword";
+	public String findPassword(Model model) {
+		if(model.getAttribute("email")==null) {
+			return "login/findPassword";
+		} else {
+			return "index";
+		}
 	}
 	
 	@PostMapping(value="/mainUserDataSave")
@@ -49,10 +67,11 @@ public class MainController {
 	
 	@PostMapping(value="/mainUserExist")
 	@ResponseBody
-	public String mainUserExist(@RequestParam("email") String email, @RequestParam("password") String password, Model model) {
+	public String mainUserExist(@RequestParam("email") String email, @RequestParam("password") String password, Model model, HttpSession session) {
 		String userExist = mainService.mainUserExist(email, password);
 		if(userExist == "exist") {
 			model.addAttribute("userExist", userExist);
+			session.setAttribute("email", email);
 		}
 		return userExist;
 	}
